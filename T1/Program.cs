158aef298c6c08585da6875bd08b5d20b605a3a7
Victoria <viktorijabulvane@gmail.com>
2015-11-16 14:57:12 +0200

    First commit Test

---
+++ T1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Test");
+
+            Console.Read();
+
+        }
+    }
+}
